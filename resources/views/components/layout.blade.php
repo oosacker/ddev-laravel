5234c7234d7ec0293ca422537b4b55ee5204681c
oosacker <oosacker@gmail.com>
2024-12-28 11:03:43 +0800

    Links logic

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,10 +18,9 @@
                             </div>
                             <div class="hidden md:block">
                                 <div class="ml-10 flex items-baseline space-x-4">
-                                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                    <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                                    <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                                    <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                    <x-navlink type="link" :active="request()->is('/')" href="/">Home</x-navlink>
+                                    <x-navlink type="link" :active="request()->is('about')" href="/about">About</x-navlink>
+                                    <x-navlink type="link" :active="request()->is('contact')" href="/contact">Contact</x-navlink>
                                 </div>
                             </div>
                         </div>
--- resources/views/components/navlink.blade.php
+++ resources/views/components/navlink.blade.php
@@ -1,1 +1,24 @@
-<a {{ $attributes }}>{{ $slot }}</a>
+{{-- Default prop state --}}
+@props([
+  'active' => false,
+  'type' => 'link'
+])
+
+@if ($type === 'link')
+  <a
+    class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}"
+    aria-current="{{ request()->is('/') ? 'page' : '' }}"
+    {{ $attributes }}
+  >
+    {{ $slot }}
+  </a>
+
+@elseif ($type === 'button')
+  <button
+    class="button"
+    {{ $attributes }}
+  >
+    {{ $slot }}
+  </button>
+
+@endif
