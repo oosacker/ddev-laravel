533ca9b9a1e8c5164043bd581c5c4b931b9d1d9f
oosacker <oosacker@gmail.com>
2024-12-30 11:38:57 +0800

    Job model in database

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,42 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-  public static function all(): array
-  {
-    return [
-        [
-            'title' => 'Backend Developer',
-            'salary' => '$3000',
-            'id' => 1
-        ],
-        [
-            'title' => 'Frontend Developer',
-            'salary' => '$2500',
-            'id' => 2
-        ],
-        [
-            'title' => 'Fullstack Developer',
-            'salary' => '$4000',
-            'id' => 3
-        ],
-        [
-            'title' => 'Developer',
-            'salary' => '$5000',
-            'id' => 4
-        ],
-    ];
-  }
-
-  public static function find(int $id): array
-  {
-    $job = Arr::first(self::all(), fn($job) => $job['id'] == $id);
-
-    if (!$job) {
-        abort(404);
-    }
-
-    return $job;
-  }
+class Job extends Model
+{
+  protected $table = 'job_listings';
+  protected $fillable = ['title', 'salary'];
 }
