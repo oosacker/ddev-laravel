b08c73fb753d912f7dcbadd942e7887070518b13
oosacker <oosacker@gmail.com>
2024-12-30 14:23:15 +0800

    Factories for employer and job

--- database/migrations/2024_12_30_040117_add_first_name_and_last_name_to_users_table.php
+++ database/migrations/2024_12_30_040117_add_first_name_and_last_name_to_users_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\Employer;
 
 return new class extends Migration
 {
@@ -12,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listings', function (Blueprint $table) {
-            $table->id();
-            $table->foreignIdFor(Employer::class);
-            $table->string('title');
-            $table->string('salary');
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name')->after('id');
+            $table->string('last_name')->after('first_name');
         });
     }
 
@@ -26,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listings');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['first_name', 'last_name']);
+        });
     }
 };
